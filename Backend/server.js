d9a6217682bdf40458d4934a4ce2f41a2718a058
WatchaphonTee <tynoy2012@gmail.com>
2024-11-02 23:37:03 +0700

    addmenu

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,7 @@ app.use("/add",require("./apis/add_api"));
 app.use("/check",require("./apis/check_api"));
 app.use("/shop",require("./apis/shop_api"));
 app.use("/order",require("./apis/order_api"));
+app.use("/show",require("./apis/show_api"));
 app.use('/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
 //server runs on port 8000
